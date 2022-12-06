d613a9295881a6e42e4af6412a9d7a23079ecb82
Dhruwang <dhruwangjariwala18@gmail.com>
2022-12-06 18:32:16 +0530

    completed home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='homeMain d-flex align-items-center'>
+        <div className='container my-auto'>
+            <h1 className='title rofontbotoBold'>MARKETO</h1>
+            <p className='rofontbotoBold fs-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button className='button fs-5 my-4'>Discover</button>
+        </div>
+    </div>
+  )
+}
