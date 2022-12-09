a58a9936974be446263ff83b8b7599f6e732ab8b
Dhruwang <dhruwangjariwala18@gmail.com>
2022-12-09 16:38:42 +0530

    made minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg px-4 ">
+            <nav class="navbar navbar-expand-lg px-4 navbar-dark ">
                 <div class="container-fluid py-2 px-4">
-                    <a class="navbar-brand text-light fs-2" href="/">Company Name</a>
+                    <a class="navbar-brand text-light fs-2" href="/">Marketo</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
